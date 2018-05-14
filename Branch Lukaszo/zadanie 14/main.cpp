c2728a339cb05d152f4a112f7efce6f54066e215
Łukasz Górski <lukasz.gg13@gmail.com>
2018-05-14 22:31:17 +0200

    zadanie 13,14,18
    zadanie 13,14,18

---
+++ Branch Lukaszo/zadanie 14/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+int ile;
+clock_t start,stop;
+double czas;
+int main()
+{
+    cout << "Ile liczb w tablicy?" << endl;
+    cin>> ile;
+    int*tablica;
+    tablica = new int[ile];
+    start = clock();
+    for(int i=0;i<ile;i++){
+        tablica[i]=i;
+        tablica[i]+=50;
+    }
+    stop=clock();
+    czas=(double)(stop-start)/CLOCKS_PER_SEC;
+    cout<<"Czas zapisu(bez  wskaznika)  "<<czas<<endl;
+    delete[] tablica;
+    tablica = new int[ile];
+    int*p=tablica;
+    start=clock();
+    for(int i=0;i<ile;i++){
+        *p =i;
+        *p +=50;
+        p++;
+    }
+stop=clock();
+czas=(double)(stop-start)/CLOCKS_PER_SEC;
+cout<<"Czas zapisu z wskaznikiem   :"<<czas<<endl;
+delete[] tablica;
+
+    return 0;
+}
