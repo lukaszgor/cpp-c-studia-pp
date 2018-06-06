8d4b6285a8b31322cdec9950acc8f8d9f2437ceb
Łukasz Górski <lukasz.gg13@gmail.com>
2018-06-06 22:45:26 +0200

    zad17 done!
    zad17 done!

--- Branch Lukaszo/zadanie 14/main.cpp
+++ Branch Lukaszo/zadanie 14/main.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-
 using namespace std;
 int ile;
-clock_t start,stop;
+clock_t start, stop;
 double czas;
-int main()
-{
-    cout << "Ile liczb w tablicy?" << endl;
-    cin>> ile;
-    int*tablica;
-    tablica = new int[ile];
-    start = clock();
-    for(int i=0;i<ile;i++){
+
+int main(){
+    cout << "Ile liczb w tablicy: ";
+    cin >> ile;
+
+        int *tablica;
+        tablica=new int [ile];
+        start=clock();
+        for (int i=0; i<ile; i++){
         tablica[i]=i;
         tablica[i]+=50;
-    }
-    stop=clock();
-    czas=(double)(stop-start)/CLOCKS_PER_SEC;
-    cout<<"Czas zapisu(bez  wskaznika)  "<<czas<<endl;
-    delete[] tablica;
-    tablica = new int[ile];
-    int*p=tablica;
-    start=clock();
-    for(int i=0;i<ile;i++){
-        *p =i;
-        *p +=50;
-        p++;
-    }
-stop=clock();
-czas=(double)(stop-start)/CLOCKS_PER_SEC;
-cout<<"Czas zapisu z wskaznikiem   :"<<czas<<endl;
-delete[] tablica;
+        }
+        stop=clock();
+        czas=(double)(stop - start) / CLOCKS_PER_SEC;
+        cout << "Czas zapisu (bez wskaznika):" << czas << endl;
+        delete [] tablica;
 
+
+        tablica=new int [ile];
+        int *p=tablica;
+        start=clock();
+        for (int i=0; i<ile; i++){
+        *p=i;
+        *p+=50;
+        p++;
+        }
+        stop=clock();
+        czas=(double)(stop-start)/CLOCKS_PER_SEC;
+        cout << "Czas zapisu (ze wskaznikiem):" << czas << endl;
+        delete [] tablica;
     return 0;
 }
--- Branch Lukaszo/zadanie 17/main.cpp
+++ Branch Lukaszo/zadanie 17/main.cpp
@@ -1,40 +1,74 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-
 using namespace std;
 int ile;
-clock_t start,stop;
+clock_t start, stop;
 double czas;
-int main()
+long double *fibo,*p;
+ int main()
 {
-
-
-    cout << "Ile liczb w tablicy?" << endl;
-    cin>> ile;
-    int*tablica;
-    tablica = new int[ile];
-    start = clock();
-    for(int i=0;i<ile;i++){
-        tablica[i]=i;
-        tablica[i]+=50;
-    }
-    stop=clock();
-    czas=(double)(stop-start)/CLOCKS_PER_SEC;
-    cout<<"Czas zapisu(bez  wskaznika)  "<<czas<<endl;
-    delete[] tablica;
-    tablica = new int[ile];
-    int*p=tablica;
+    int n = 1;
+    cout<<"wpisz numer liczby Fibo: "<<endl;
+    cin>>n;
+    long long int fib[n];
+    fib[0] = 1;
+    fib[1] = 1;
     start=clock();
-    for(int i=0;i<ile;i++){
-        *p =i;
-        *p +=50;
-        p++;
+    for(int i=2; i<=n; i++)
+    {
+    fib[i] = fib[i-1] + fib[i-2];
+    }
+    for(int i=0; i<=n; i++)
+    {
+        //cout<<fib[i]<<", "<<endl;
     }
 stop=clock();
-czas=(double)(stop-start)/CLOCKS_PER_SEC;
-cout<<"Czas zapisu z wskaznikiem   :"<<czas<<endl;
-delete[] tablica;
+czas=(double)(stop - start) / CLOCKS_PER_SEC;
+cout << "Czas zapisu Tablica dynamiczna:" << czas << endl;
+
+////////////////////////////////////////////// zad 16
+  start=clock();
+ if(n<0)
+        cout<<endl<<"milego dnia"<<endl;
+    else{
+        fibo=new long double[n];
+        p=fibo;
+        *p=1;
+        //cout<<endl<<"[0]->"<<(int)p<<endl;
+        *(++p)=1;
+        //cout<<"[1]->"<<(int)p<<endl;
+        p++;
+        for(int i=2;i<n;i++){
+            //cout<<"{"<<i<<"}"<<(int)(p+(i-2))<<"--";
+            //cout<<(int)(p+(i-3))<<"+"<<(int)(p+(i-4))<<endl;
+         *(p+(i-2))=*(p+(i-3))+*(p+(i-4));
+        }
+
+        p=fibo;
+        //cout<<"adres 1 elementu"<<int(p)<<endl;
+        p=&fibo[n-1];
+        //cout<<"adres 1 elementu"<<int(p)<<endl;
+        for(int i =0;i<n;i++){
+            //cout<<endl<<"Fib |"<<i+1<<"\  "<<*p;
+            p++;
+        }
+        p=&fibo[n-1];
+        //cout<<endl<<"zlota liczba"<<(*p)/(*(--p))<<endl;
+        delete []fibo;
+        //cout<<endl<<endl;
+stop=clock();
+czas=(double)(stop - start) / CLOCKS_PER_SEC;
+cout << "Czas zapisu wskazniki:" << czas << endl;
+
+    }
+
+
+
+//////////////////////////////////////////////
+
+
+
 
     return 0;
 }
